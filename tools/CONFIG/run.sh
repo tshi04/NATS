d6b1ef7d7764a2fdc88bea5279fe25587398fd15
Ubuntu <ubuntu@ip-172-31-25-13.ec2.internal>
2018-06-08 03:44:56 +0000

    python3_setup_aws

--- tools/CONFIG/run.sh
+++ tools/CONFIG/run.sh
@@ -1,13 +1,13 @@
 sudo apt-get update
-sudo apt install python
-sudo apt install python-pip
-pip install --upgrade pip
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install sklearn
-sudo pip install matplotlib
-sudo pip install argparse
-sudo pip install glob2
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install numpy
+sudo pip3 install scipy
+sudo pip3 install sklearn
+sudo pip3 install matplotlib
+sudo pip3 install argparse
+sudo pip3 install glob2
 
 sudo apt install nvidia-settings
 nvidia-settings --version
--- tools/CONFIG/run2.sh
+++ tools/CONFIG/run2.sh
@@ -1,11 +1,12 @@
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/361.42/NVIDIA-Linux-x86_64-361.42.run
 chmod u+x NVIDIA-Linux-x86_64-361.42.run
 sudo ./NVIDIA-Linux-x86_64-361.42.run
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
+sudo apt install nvidia-cuda-toolkit
 
-sudo pip install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp27-cp27mu-linux_x86_64.whl 
-sudo pip install torchvision 
-
+sudo pip3 install http://download.pytorch.org/whl/cu90/torch-0.4.0-cp35-cp35m-linux_x86_64.whl 
+sudo pip3 install torchvision
