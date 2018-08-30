6efcc8847cb9bf79f6c457d7304ce0efca2b536b
tshi04 <tshi@vt.edu>
2018-08-30 21:52:04 +0000

    rouge_package

--- tools/rouge_package/run.sh
+++ tools/rouge_package/run.sh
@@ -1,9 +1,13 @@
-
 cp ROUGE-1.5.5.tar.gz ~
 tar -xvf ~/ROUGE-1.5.5.tar.gz --directory ~
 rm ~/ROUGE-1.5.5.tar.gz
 sudo apt install libxml-parser-perl
 sudo pip3 install pyrouge
+
+cd ~
+mkdir .pyrouge
+echo [pyrouge settings] >> .pyrouge/settings.ini
+echo home_dir = $PWD/ROUGE-1.5.5 >> .pyrouge/settings.ini
 
 cd ~/ROUGE-1.5.5/data/
 rm WordNet-2.0.exc.db
